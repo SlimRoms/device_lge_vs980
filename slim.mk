776d5a76b858144f8b6d9857f3a31052d80f878b
Griffin Millender <griffinn.millender@gmail.com>
2013-11-25 15:17:36 -0600

    Cleanup
    Change-Id: I5757f264559a4d06af3bb2b9a48d693072f56ded

--- slim.mk
+++ slim.mk
@@ -19,7 +19,7 @@ $(call inherit-product, vendor/slim/config/common_ledflash.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vs980
-PRODUCT_NAME := cm_vs980
+PRODUCT_NAME := slim_vs980
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-VS980
 PRODUCT_MANUFACTURER := lge
