0d68aea7a7fabc7683ebd44d378db07a42736ea3
Rashed Abdel-Tawab <rashed@linux.com>
2014-11-16 00:15:48 -0500

    vs980: Add properties to fix data
    Change-Id: Ibc3054d4c857deaf1ad6d47fce4291a7bc34c79f

--- device.mk
+++ device.mk
@@ -25,7 +25,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.telephony.default_network=10 \
 	ro.cdma.home.operator.numeric=311480 \
 	ro.cdma.home.operator.alpha=Verizon \
-	ro.cdma.homesystem=64,65,76,77,78,79,80,81,82,83
+	ro.cdma.homesystem=64,65,76,77,78,79,80,81,82,83 \
+	ro.cdma.subscribe_on_ruim_ready=true \
+	ro.ril.svdo=true
 
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
