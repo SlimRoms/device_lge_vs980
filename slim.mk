cb807dcc2d6357360cdf59d92c0fa61ced4f54f8
Griffin Millender <griffinn.millender@gmail.com>
2015-05-31 11:14:42 -0500

    Changes for slim

--- slim.mk
+++ slim.mk
@@ -2,18 +2,18 @@
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Slim stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/vs980/vs980.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vs980
-PRODUCT_NAME := cm_vs980
+PRODUCT_NAME := slim_vs980
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-VS980
 PRODUCT_MANUFACTURER := lge
@@ -24,4 +24,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 PRODUCT_GMS_CLIENTID_BASE := android-verizon
 
-PRODUCT_PACKAGES += Torch
